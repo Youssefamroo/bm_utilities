61fd12833c4de2bf1c775eccbd9a44f4bc73c797
a7medma7amad <85848334+a7medma7amad@users.noreply.github.com>
2023-05-15 22:59:15 +0300

    quality_gates added

--- bm_utilities/bm_utilities.py
+++ bm_utilities/bm_utilities.py
@@ -1,7 +1,9 @@
 from Address_finder import AddressFinder as af
 import geopandas as gpd
 import pandas as pd
+import quality_gates as qg
 def address_finder(geodf,address):
     construct = af.AddressFinder(geodf)
     address = af.find_address(address)
     return address
+
