6b01afb21708912daf35945271c06588de2786fa
Youssefamroo <usef.amr11@gmail.com>
2023-05-18 11:08:24 +0300

    init change

--- bm_utilities/bm_utilities.py
+++ bm_utilities/bm_utilities.py
@@ -1,6 +1,7 @@
 from Address_finder import AddressFinder as af
 import geopandas as gpd
 import pandas as pd
+import quality_gates as qg
 def address_finder(geodf,address):
     construct = af(geodf)
     address = construct.find_address(address)
@@ -16,9 +17,6 @@ def quality_gates_check(df,config):
 #print(address_finder( gpd.read_file(r"D:\\Task_2\\point of interst\\Reading shape files\\egy_admbnda_adm3_capmas_20170421.shp"),
 #                     pd.read_csv(r"D:\\Task_2\\address_finder_application\\output\\Address_Finder\\Address Finder\\test_data.csv")))
 
-geo_df = gpd.read_file(r"D:\\Task_2\\point of interst\\Reading shape files\\egy_admbnda_adm3_capmas_20170421.shp")
-address = pd.read_csv(r"D:\\Task_2\\test data\\test_data.csv")
-print(address_finder(geo_df,address))
 #for i in range(Branches_lat_long.shape[0]):
 #    if((Branches_lat_long[['lat']].iloc[i,:]).isna()):
 #        continue
